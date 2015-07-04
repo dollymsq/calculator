fc1345c916df0f5d3b2cd77de5f0d672f7c9ef73
Dolly Ma <dollietmsq@gmail.com>
2015-07-04 01:07:43 -0400

    first class over

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var isInTyingState: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if(isInTyingState){
+            display.text = display.text! + digit
+            println("digit = \(digit)")
+        }
+        else{
+            display.text = digit;
+            isInTyingState = true;
+        }
+        
     }
 
 
