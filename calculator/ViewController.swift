ad1fd301ad97f55989b5e9ef99cd4a40e04b6144
Dolly Ma <dollietmsq@gmail.com>
2015-07-06 22:54:36 -0400

    second course done

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController
         let digit = sender.currentTitle!
         if(isInTyingState){
             display.text = display.text! + digit
-            println("digit = \(digit)")
         }
         else{
             display.text = digit;
@@ -26,7 +25,56 @@ class ViewController: UIViewController
         }
         
     }
-
-
+    
+    var operandStack = Array<Double>();
+    @IBAction func enter() {
+        isInTyingState = false;
+        operandStack.append(displayValue)
+        println("\(operandStack)")
+    }
+    
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            isInTyingState = false;
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if(isInTyingState){
+            enter()
+        }
+        switch operation {
+            case "+":
+                calculation {$0 + $1}
+            case "−":
+                calculation {$1 - $0}
+            case "×":
+                calculation {$0 * $1}
+            case "÷":
+                calculation {$1 / $0}
+            case "√":
+                calculation {sqrt($0)}
+        default: break
+        }
+    }
+    
+    func calculation(operation: (Double, Double) -> Double){
+        if(operandStack.count >= 2){
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func calculation(operation: Double -> Double){
+        if(operandStack.count >= 1){
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
 }
 
